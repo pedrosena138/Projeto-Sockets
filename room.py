8dbce2cd1f66de84d6d17b9de31656191425b8a1
Alisson Diego <55363179+diegodnz@users.noreply.github.com>
2019-12-03 14:37:41 -0300

    Update room.py

--- room.py
+++ room.py
@@ -2,7 +2,7 @@ import threading
 import time
 from socket import *
 
-class CheckADM(threading.Thread):
+class CheckADM(threading.Thread):  # Esta thread é executada por todos os membros da sala, menos o adm. Ela checa se o adm se desconectou
     def __init__(self, room, myNick, client):
         threading.Thread.__init__(self)
         self.room = room
@@ -11,6 +11,7 @@ class CheckADM(threading.Thread):
         self.client = client
 
     def run(self):
+        """Checa continuamente se o adm está conectado, caso ele seja desconectado, roda o método changeADM"""
         check = True
         while check:
             if not self.client.running or self.client.banned:
@@ -27,7 +28,8 @@ class CheckADM(threading.Thread):
                 self.changeADM()
 
     def changeADM(self):
-        try:
+        """Neste método, o adm atual é trocado pelo primeiro membro da lista 'self.room.queueADM', então todos os membros saberão quem será o novo adm"""
+        try:  # Remover o atual adm está no try, pois caso o adm tenha saído da sala avisando a todos, ele já foi removido. Porém se ele saiu sem avisar, é necessário removê-lo
             ipADM = self.members[self.room.nickADM][0]
             portADM = self.members[self.room.nickADM][1]
             self.members.pop(self.room.nickADM)
@@ -35,16 +37,17 @@ class CheckADM(threading.Thread):
         except:
             pass
 
-        newADM = self.room.queueADM.pop(0)
+        newADM = self.room.queueADM.pop(0)  # O novo adm é o primeiro membro da fila self.room.queueADM
         self.room.nickADM = newADM
         self.room.ipADM = self.members[newADM][0]
         self.room.portADM = self.members[newADM][1]
         print('O dono da sala se desconectou!!!')
-        if not self.myNick == newADM:
+        if not self.myNick == newADM:  # Se o novo adm não for este próprio cliente, então ele voltará a rodar a thread de checagem de adm
             print(f'{newADM} é o novo dono da sala!!')
             self.run()
-        else:
+        else:  # Caso o novo adm seja este próprio cliente, agora ele não rodará mais esta thread e passará a fazer a checagem dos membros
             print('-'*30 + '\nVocê é o novo dono da sala!!\n' + '-'*30)
+            print(f'\nA sala agora está no seu endereço:\nIP: {self.client.ip}\nPorta: {self.client.port}')
             print('\nComandos administrativos:'
                   '\n/ban nick    -> Para banir um membro da sala'
                   '\n/kick nick   -> Para expulsar um membro da sala\n')
@@ -52,13 +55,14 @@ class CheckADM(threading.Thread):
             chekingMembers = CheckMembers(self.room, self.client)
             chekingMembers.start()
 
-class CheckMembers(threading.Thread):
+class CheckMembers(threading.Thread):  # Esta thread é executada somente pelo adm, na qual ele checa continuamente se algum membro se desconectou
     def __init__(self, room, client):
         threading.Thread.__init__(self)
         self.room = room
         self.client = client
 
     def run(self):
+        """Checa continuamente se algum membro foi desconectado da sala. Caso encontre, envia esta informação para toda a sala"""
         while True:
             time.sleep(0.1)
             if not self.client.running or self.client.banned:
@@ -77,6 +81,7 @@ class CheckMembers(threading.Thread):
                     time.sleep(1)
 
     def memberDisconnect(self, member):
+        """Remove o membro desconectado da sala e envia um update de sala para todos os membros"""
         try:
             ipMember, portMember = self.room.members[member]
             self.room.members.pop(member)
